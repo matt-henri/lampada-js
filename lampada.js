bd97159f1524e27270b5c198ee274a3de3e74a27
Mateus Henrique <mathmarkub@gmail.com>
2023-01-02 19:38:02 -0300

    Update lampada.js

--- lampada.js
+++ lampada.js
@@ -2,32 +2,35 @@ const turnOn = document.getElementById ( 'turnOn' )
 const turnOff = document.getElementById ( 'turnOff' )
 const lamp = document.getElementById ( 'lamp' )
 
-function ifBroken() {
+function itsBroken() {
     return lamp.src.indexOf ('quebrada') > -1
 }
 
 
+function lampBroken() {
+    if (!itsBroken() ) {
+        lamp.src = 'img/quebrada.jpg'
+    }
+}
+
 function lampOn() {
-    if ( !ifBroken() ) {
-        lamp.src = './img/ligada.jpg'
-    } 
+    if ( !itsBroken() ) {
+        lamp.src = 'img/ligada.jpg'
+    }
 }
 
 function lampOff() {
-    if ( !ifBroken() ) {
-        lamp.src = './img/desligada.jpg'
+    if ( !itsBroken() ) {
+        lamp.src = 'img/desligada.jpg'
     }
-}
-
-function lampBroken() {
-    if ( !ifBroken() ) {
-        lamp.src = './img/quebrada.jpg'
-    }
+    
 }
 
 
-turnOn.addEventListener ( 'click', lampOn )
-turnOff.addEventListener ( 'click', lampOff )
+turnOn.addEventListener ( 'click', lampOn)
+turnOff.addEventListener ( 'click', lampOff)
 lamp.addEventListener ( 'mouseover', lampOn)
 lamp.addEventListener ( 'mouseleave', lampOff)
 lamp.addEventListener ( 'mouseup', lampBroken)
+
+
